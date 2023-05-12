400b59688f09f2fc898aa9556ab3bfa2d6c1b4d7
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2023-05-12 09:56:34 -0300

    Add files via upload

---
+++ questao1.py
@@ -0,0 +1,27 @@
+## Questão 1 - Escreva um programa que leia uma expressão matemática 
+# na forma de string e utilize uma pilha para verificar se os parênteses estão balanceados.
+
+from pilha import Pilha
+
+def verificarparenteses(exp):
+    p = Pilha()
+    num = '0123456789'
+    expressao = []
+    
+    for i in exp:
+        if i in num:
+            expressao.append(i)
+        elif i == '(':
+            p.inserir(i)
+        elif i == ')':
+            p.inserir(i)
+            print('Parênteses balanceados.')
+        else:
+            print('Parênteses não balanceados.')
+    while not p.is_empty():
+        expressao.append(p.remover())
+    return ''.join(expressao)
+
+s = str(input('Expressão: '))
+verificarparenteses(s)
+
